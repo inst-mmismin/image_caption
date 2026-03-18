b38a4e57ef9958e3cd9930824b0beb606a760ff1
inst.mmismin <inst.mmismin@gmail.com>
2026-03-18 10:57:10 +0900

    [Add] 실행 shell 파일

---
+++ run.sh
@@ -0,0 +1,45 @@
+python step1.py --epochs 100 \
+                --learning_rate 0.01 \
+                --min_lr 0.0001 \
+                --batch_size 512 \
+                --use_contrastive \
+                --proj_type linear \
+                --use_layer_norm 
+
+python step1.py --epochs 100 \
+                --learning_rate 0.01 \
+                --min_lr 0.0001 \
+                --batch_size 512 \
+                --use_contrastive \
+                --proj_type mlp \
+                --use_layer_norm 
+
+python step1.py --epochs 100 \
+                --learning_rate 0.01 \
+                --min_lr 0.0001 \
+                --batch_size 512 \
+                --use_contrastive \
+                --weight_contrastive 1 \
+                --proj_type mlp \
+                --use_layer_norm 
+
+python step1.py --epochs 100 \
+                --learning_rate 0.01 \
+                --min_lr 0.0001 \
+                --batch_size 512 \
+                --use_contrastive \
+                --contra_temp 0.05 \
+                --proj_type linear \
+                --use_layer_norm 
+
+
+python step1.py --epochs 100 \
+                --learning_rate 0.01 \
+                --min_lr 0.0001 \
+                --batch_size 512 \
+                --use_contrastive \
+                --weight_contrastive 1 \
+                --contra_temp 0.05 \
+                --proj_type linear \
+                --use_layer_norm 
+
